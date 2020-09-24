e6b83858118b11b5e4aa5b3858404a7b90424637
bshubham24 <acer@DESKTOP-QLPFQS8>
2020-09-24 12:48:23 +0530

    [Shubham]Added_UC1

--- src/com/capgi/EmployeeWageBuilder.java
+++ src/com/capgi/EmployeeWageBuilder.java
@@ -3,6 +3,16 @@ package com.capgi;
 public class EmployeeWageBuilder {
    public static void main(String[] args) {
 	   
-	   System.out.println("Welcome");
-    }
+		int IS_FULLTIME_EMPLOYEE = 1;
+		
+		double CHECK = Math.floor(Math.random()*10)%2;
+		
+		if(CHECK== IS_FULLTIME_EMPLOYEE) {
+			System.out.println("FULLTIME EMPLOYEE");
+		}
+		else {
+			System.out.println("NOT A FULLTIME EMPLOYEE");
+		}
+	
+   }
 }
