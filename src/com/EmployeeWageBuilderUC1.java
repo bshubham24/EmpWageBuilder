4788dbab10c57b55efd214afb81040f032cf6c81
bshubham24 <acer@DESKTOP-QLPFQS8>
2020-09-24 12:41:53 +0530

    Welcome

---
+++ src/com/EmployeeWageBuilderUC1.java
@@ -0,0 +1,18 @@
+
+	package com;
+
+	public class EmployeeWageBuilderUC1 {
+	     public static void main(String[] args) {
+			int IS_EMPLOYEE = 1;
+			double CHECK = Math.floor(Math.random()*10)%2;
+			if(CHECK== IS_EMPLOYEE	) {
+				System.out.println("Employee is permanent");
+			}
+			else {
+				System.out.println("Not an permanent employee");
+			}
+			
+		}
+	}
+	  
+
---
+++ src/com/capgi/EmployeeWageBuilder.java
@@ -0,0 +1,8 @@
+package com.capgi;
+
+public class EmployeeWageBuilder {
+   public static void main(String[] args) {
+	   
+	   System.out.println("Welcome");
+    }
+}
