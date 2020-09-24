922efa3959066d482579f33a4b187d7a48eed40e
bshubham24 <acer@DESKTOP-QLPFQS8>
2020-09-24 13:00:35 +0530

    [

--- src/com/capgi/EmployeeWageBuilder.java
+++ src/com/capgi/EmployeeWageBuilder.java
@@ -5,24 +5,25 @@ public class EmployeeWageBuilder {
 	   
 		int IS_FULLTIME_EMPLOYEE = 1;
 		
-		double Check = Math.floor(Math.random()*10)%2;
-		
-	
-	
-		  
-			int EMP_RATE_PER_HOUR = 20;
-			int emp_Hrs=0;
-			int emp_Wage=0;
-		
-			if(Check== IS_FULLTIME_EMPLOYEE) {
-				emp_Hrs = 8;
-			}
-			else {
-				emp_Hrs = 0;
-			}
-			emp_Wage = emp_Hrs*EMP_RATE_PER_HOUR;
-			System.out.println(emp_Wage);
-			
+
+   
+	    int IS_PARTTIME_EMPLOYEE = 2;
+		int EMP_RATE_PER_HOUR = 20;
+		int emp_Hrs=0;
+		int emp_Wage=0;
+		double Check= Math.floor(Math.random()*10)%3;
+		if(Check== IS_FULLTIME_EMPLOYEE) {
+			emp_Hrs = 8;
 		}
+		else if(Check== IS_PARTTIME_EMPLOYEE) {
+			emp_Hrs =4;
+		}
+		else {
+			emp_Hrs = 0;
+		}
+		emp_Wage = emp_Hrs*EMP_RATE_PER_HOUR;
+		System.out.println(emp_Wage);
+		
+	}
    
 }
